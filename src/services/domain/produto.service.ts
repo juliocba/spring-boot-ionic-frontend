053b65fa7ece7346deb402843bc03dae83b51fc7
Julio Baltazar <julio.baltazar@ifactory.com.br>
2019-07-04 09:40:38 -0300

    carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { Observable } from "rxjs/Rx";
+import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService {
 
     constructor(public http: HttpClient) {
+    }
+
+    findById(produtoId: string) {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produtoId}`);
     }
 
     findByCategoria(categoriaId: string) {
@@ -18,5 +23,9 @@ export class ProdutoService {
         return this.http.get(url, { responseType: 'blob' });
     }
 
+    getImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
+        return this.http.get(url, { responseType: 'blob' });
+    }
 
 }
